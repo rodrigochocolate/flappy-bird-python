442b7db7d3b01f0ef2a7a93ba12b11234311cb7b
Rodrigo <rodrigodev2021@gmail.com>
2024-07-26 17:09:09 -0300

    Flappy Bird v1.0

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import *
 
 class Bird(pygame.sprite.Sprite):
@@ -14,6 +15,7 @@ class Bird(pygame.sprite.Sprite):
         self.current_image = 0
 
         self.image = pygame.image.load('images/bluebird-upflap.png').convert_alpha()
+        self.mask = pygame.mask.from_surface((self.image))
         self.rect = self.image.get_rect()
         self.rect[0] = SCREEN_WIDTH / 2
         self.rect[1] = SCREEN_HEIGHT / 2
@@ -30,10 +32,62 @@ class Bird(pygame.sprite.Sprite):
     def bump(self):
         self.speed = -SPEED
 
+class Pipe(pygame.sprite.Sprite):
+
+    def __init__(self, inverted, xpos, ysize):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('images/pipe-green.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (PIPE_WIDTH, PIPE_HEIGHT))
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+
+        if inverted:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect[1] = - (self.rect[3] - ysize)
+        else:
+            self.rect[1] = SCREEN_HEIGHT - ysize
+
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self):
+        self.rect[0] -= GAME_SPEED
+
+class Ground(pygame.sprite.Sprite):
+
+    def __init__(self, xpos):
+        super().__init__()
+        self.image = pygame.image.load('images/base.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (GROUND_WIDTH, GROUND_HEIGHT))
+        self.mask = pygame.mask.from_surface((self.image))
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+        self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
+
+    def update(self):
+        self.rect[0] -= GAME_SPEED
+
+def is_off_screen(sprite):
+    return sprite.rect[0] < -(sprite.rect[2])
+
+def get_random_pipes(xpos):
+    size = random.randint(100, 300)
+    pipe = Pipe(False, xpos, size)
+    pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
+    return (pipe, pipe_inverted)
+
 SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 700
+SCREEN_HEIGHT = 800
 SPEED = 10
 GRAVITY = 1
+GAME_SPEED = 10
+GROUND_WIDTH = 2 * SCREEN_WIDTH
+GROUND_HEIGHT = 100
+PIPE_WIDTH = 80
+PIPE_HEIGHT = 500
+PIPE_GAP = 200
 
 pygame.init()
 pygame.display.set_caption('Flappy Bird')
@@ -42,10 +96,23 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 BACKGROUND = pygame.image.load('images/background-day.png')
 
 BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
 bird_group = pygame.sprite.Group()
 bird = Bird()
-
 bird_group.add(bird)
+
+pipe_group = pygame.sprite.Group()
+for i in range(2):
+    pipes = get_random_pipes(SCREEN_WIDTH * i + 800)
+    pipe_group.add(pipes[0])
+    pipe_group.add(pipes[1])
+
+ground_group = pygame.sprite.Group()
+for i in range(2):
+    ground = Ground(GROUND_WIDTH * i)
+    ground_group.add(ground)
+
+
 
 clock = pygame.time.Clock()
 
@@ -61,8 +128,31 @@ while True:
 
     screen.blit(BACKGROUND, (0, 0))
 
+    if is_off_screen(ground_group.sprites()[0]):
+        ground_group.remove(ground_group.sprites()[0])
+
+        new_ground = Ground(GROUND_WIDTH - 20)
+        ground_group.add(new_ground)
+
+    if is_off_screen(pipe_group.sprites()[0]):
+        pipe_group.remove(pipe_group.sprites()[0])
+        pipe_group.remove(pipe_group.sprites()[0])
+
+        pipes = get_random_pipes(SCREEN_WIDTH * 2)
+
+        pipe_group.add(pipes[0])
+        pipe_group.add(pipes[1])
+
     bird_group.update()
+    pipe_group.update()
+    ground_group.update()
 
     bird_group.draw(screen)
+    pipe_group.draw(screen)
+    ground_group.draw(screen)
 
     pygame.display.update()
+
+    if pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask):
+        # Game Over
+        break
